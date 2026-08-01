6122f3c578e794aa1c1c1140f68c1d5a683c8f84
Abithavenkat2004 <abithavenkat39@gmail.com>
2026-08-01 14:37:47 +0530

    Add initial data for vendors, products, and users
    Inserted multiple vendors, products, and users into the database.

---
+++ 02_Insert_Data.sql
@@ -0,0 +1,25 @@
+-- Insert Vendors
+INSERT INTO VENDORS
+VALUES (101,'ABC Suppliers','Rajesh','9876543210','abc@gmail.com','Chennai');
+
+INSERT INTO VENDORS
+VALUES (102,'XYZ Traders','Suresh','9876543211','xyz@gmail.com','Bangalore');
+
+-- Insert Products
+INSERT INTO PRODUCTS
+VALUES (201,'Laptop','Electronics',55000,20,101);
+
+INSERT INTO PRODUCTS
+VALUES (202,'Printer','Electronics',12000,10,101);
+
+INSERT INTO PRODUCTS
+VALUES (203,'Keyboard','Accessories',800,100,102);
+
+-- Insert Users
+INSERT INTO USERS
+VALUES (301,'Abi','abi@gmail.com','Employee','9876543222');
+
+INSERT INTO USERS
+VALUES (302,'Manager','manager@gmail.com','Manager','9876543233');
+
+COMMIT;
